c218894aca3e86542ec93fd28e19eba7badae716
SamuelOkwalinga <sookwalinga@gmail.com>
2021-06-29 10:53:47 +0300

    first commit

---
+++ src/routes.js
@@ -0,0 +1,98 @@
+// core components
+import Dashboard from "views/admin/Dashboard.js";
+import Login from "views/auth/Login.js";
+import Maps from "views/admin/Maps.js";
+import Profile from "views/admin/Profile.js";
+import Register from "views/auth/Register.js";
+import Tables from "views/admin/Tables.js";
+// @material-ui/icons components
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import FlashOn from "@material-ui/icons/FlashOn";
+import FormatListBulleted from "@material-ui/icons/FormatListBulleted";
+import LocationOn from "@material-ui/icons/LocationOn";
+import Person from "@material-ui/icons/Person";
+import People from "@material-ui/icons/People";
+import Tv from "@material-ui/icons/Tv";
+import VpnKey from "@material-ui/icons/VpnKey";
+
+var routes = [
+  {
+    href: "#pablo",
+    name: "Upgrade to pro",
+    icon: FlashOn,
+    upgradeToPro: true,
+  },
+  {
+    path: "/index",
+    name: "Dashboard",
+    icon: Tv,
+    iconColor: "Primary",
+    component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/suppliers",
+    name: "Suppliers",
+    icon: People,
+    iconColor: "success",
+    component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/loaders",
+    name: "Loaders",
+    icon: People,
+    iconColor: "success",
+    component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/invoices",
+    name: "Invoices",
+    icon: FormatListBulleted,
+    iconColor: "Warning",
+    component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/maps",
+    name: "Maps",
+    icon: LocationOn,
+    iconColor: "Warning",
+    component: Maps,
+    layout: "/admin",
+  },
+  {
+    path: "/user-profile",
+    name: "User Profile",
+    icon: Person,
+    iconColor: "WarningLight",
+    component: Profile,
+    layout: "/admin",
+  },
+  {
+    path: "/tables",
+    name: "Acitivites",
+    icon: FormatListBulleted,
+    iconColor: "Error",
+    component: Tables,
+    layout: "/admin",
+  },
+  {
+    path: "/login",
+    name: "Login",
+    icon: VpnKey,
+    iconColor: "Info",
+    component: Login,
+    layout: "/auth",
+  },
+  {
+    path: "/register",
+    name: "Register",
+    icon: AccountCircle,
+    iconColor: "ErrorLight",
+    component: Register,
+    layout: "/auth",
+  },
+];
+export default routes;
---
+++ src/views/admin/Dashboard.js
@@ -0,0 +1,192 @@
+import React from "react";
+// javascipt plugin for creating charts
+import Chart from "chart.js";
+// react plugin used to create charts
+import { Line, Bar } from "react-chartjs-2";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
+import Box from "@material-ui/core/Box";
+import Button from "@material-ui/core/Button";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import CardHeader from "@material-ui/core/CardHeader";
+import Container from "@material-ui/core/Container";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+// core components
+import Header from "components/Headers/Header.js";
+
+import {
+  chartOptions,
+  parseOptions,
+  chartExample1,
+  chartExample2,
+} from "variables/charts.js";
+
+import componentStyles from "assets/theme/views/admin/dashboard.js";
+
+const useStyles = makeStyles(componentStyles);
+
+function Dashboard() {
+  const classes = useStyles();
+  const theme = useTheme();
+  const [activeNav, setActiveNav] = React.useState(1);
+  const [chartExample1Data, setChartExample1Data] = React.useState("data1");
+
+  if (window.Chart) {
+    parseOptions(Chart, chartOptions());
+  }
+
+  const toggleNavs = (index) => {
+    setActiveNav(index);
+    setChartExample1Data("data" + index);
+  };
+  return (
+    <>
+      <Header />
+      {/* Page content */}
+      <Container
+        maxWidth={false}
+        component={Box}
+        marginTop="-6rem"
+        classes={{ root: classes.containerRoot }}
+      >
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            xl={8}
+            component={Box}
+            marginBottom="3rem!important"
+            classes={{ root: classes.gridItemRoot }}
+          >
+            <Card
+              classes={{
+                root: classes.cardRoot + " " + classes.cardRootBgGradient,
+              }}
+            >
+              <CardHeader
+                subheader={
+                  <Grid
+                    container
+                    component={Box}
+                    alignItems="center"
+                    justifyContent="space-between"
+                  >
+                    <Grid item xs="auto">
+                      <Box
+                        component={Typography}
+                        variant="h6"
+                        letterSpacing=".0625rem"
+                        marginBottom=".25rem!important"
+                        className={classes.textUppercase}
+                      >
+                        <Box component="span" color={theme.palette.gray[400]}>
+                          Overview
+                        </Box>
+                      </Box>
+                      <Box
+                        component={Typography}
+                        variant="h2"
+                        marginBottom="0!important"
+                      >
+                        <Box component="span" color={theme.palette.white.main}>
+                          Packages Delivered
+                        </Box>
+                      </Box>
+                    </Grid>
+                    <Grid item xs="auto">
+                      <Box
+                        justifyContent="flex-end"
+                        display="flex"
+                        flexWrap="wrap"
+                      >
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          component={Box}
+                          marginRight="1rem!important"
+                          onClick={() => toggleNavs(1)}
+                          classes={{
+                            root:
+                              activeNav === 1
+                                ? ""
+                                : classes.buttonRootUnselected,
+                          }}
+                        >
+                          Month
+                        </Button>
+                        <Button
+                          variant="contained"
+                          color="primary"
+                          onClick={() => toggleNavs(2)}
+                          classes={{
+                            root:
+                              activeNav === 2
+                                ? ""
+                                : classes.buttonRootUnselected,
+                          }}
+                        >
+                          Week
+                        </Button>
+                      </Box>
+                    </Grid>
+                  </Grid>
+                }
+                classes={{ root: classes.cardHeaderRoot }}
+              ></CardHeader>
+              <CardContent>
+                <Box position="relative" height="350px">
+                  <Line
+                    data={chartExample1[chartExample1Data]}
+                    options={chartExample1.options}
+                    getDatasetAtEvent={(e) => console.log(e)}
+                  />
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={12} xl={4}>
+            <Card classes={{ root: classes.cardRoot }}>
+              <CardHeader
+                title={
+                  <Box component="span" color={theme.palette.gray[600]}>
+                    Performane
+                  </Box>
+                }
+                subheader="Average Delivery Time"
+                classes={{ root: classes.cardHeaderRoot }}
+                titleTypographyProps={{
+                  component: Box,
+                  variant: "h6",
+                  letterSpacing: ".0625rem",
+                  marginBottom: ".25rem!important",
+                  classes: {
+                    root: classes.textUppercase,
+                  },
+                }}
+                subheaderTypographyProps={{
+                  component: Box,
+                  variant: "h2",
+                  marginBottom: "0!important",
+                  color: "initial",
+                }}
+              ></CardHeader>
+              <CardContent>
+                <Box position="relative" height="350px">
+                  <Bar
+                    data={chartExample2.data}
+                    options={chartExample2.options}
+                  />
+                </Box>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
+}
+
+export default Dashboard;
